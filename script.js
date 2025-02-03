e44ba46f99fbe292a3f2e71709f5c7298459f713
HussainAhmad11 <145200648+HussainAhmad11@users.noreply.github.com>
2025-02-03 19:51:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var typed = new Typed(".multiple-text", {
+    strings: [
+        "Frontend Developer",
+        "MVC Developer",
+        "SQL Server Specialist",
+        "Creative Designer",
+        "Problem Solver"
+    ],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1500,
+    loop: true,
+    startDelay: 500,
+    showCursor: true,
+    cursorChar: "|"
+});
